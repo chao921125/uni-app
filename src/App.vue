2106ad2a19da7c28fdff4b86e103c56bceda0e1e
黄超 <chao.huang@advokategroup.com>
2021-07-06 16:27:02 +0800

    add demo & update file

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,45 @@
         });
       });
       */
+      // #ifdef APP-PLUS
+      // App平台检测升级，服务端代码是通过uniCloud的云函数实现的，详情可参考：https://ext.dcloud.net.cn/plugin?id=2226
+      if(plus.runtime.appid !== 'HBuilder'){ // 真机运行不需要检查更新，真机运行时appid固定为'HBuilder'，这是调试基座的appid
+        uni.request({
+          url: 'https://7a3e3fa9-7820-41d0-be80-11927ac2026c.bspapp.com/http/update', //检查更新的服务器地址
+          data: {
+            appid: plus.runtime.appid,
+            version: plus.runtime.version,
+            imei: plus.device.imei
+          },
+          success: (res) => {
+            if (res.statusCode == 200 && res.data.isUpdate) {
+              // 提醒用户更新
+              uni.showModal({
+                title: '更新提示',
+                content: res.data.note ? res.data.note : '是否选择更新',
+                success: (ee) => {
+                  if (ee.confirm) {
+                    plus.runtime.openURL(res.data.url);
+                  }
+                }
+              })
+            }
+          }
+        })
+      }
+      // 一键登录预登陆，可以显著提高登录速度
+      uni.preLogin({
+        provider: 'univerify',
+        success: (res) => {
+          // 成功
+          console.log("preLogin success: ", res);
+        },
+        fail: (res) => {
+          // 失败
+          console.log("preLogin fail res: ", res);
+        }
+      })
+      // #endif
       let userInfo = uni.getStorageSync('userInfo') || '';
       if(userInfo.id){
         //更新登陆状态
---
+++ test/demo/src/pages/extUI/datetime-picker/datetime-picker.vue
@@ -0,0 +1,170 @@
+<template>
+	<view class="page">
+		<text class="example-info">可以同时选择日期和时间的选择器</text>
+		<uni-section :title="'日期用法：' + single" type="line"></uni-section>
+		<view class="example-body">
+			<uni-datetime-picker type="date" start="2021-3-20" end="2021-5-20" @change="change" />
+		</view>
+		<uni-section :title="'时间戳用法：' + single" type="line"></uni-section>
+		<view class="example-body">
+			<uni-datetime-picker returnType="timestamp" v-model="single" start="2021-3-20" end="2021-5-20" />
+		</view>
+		<uni-section :title="'日期时间用法：' + datetimesingle" type="line"></uni-section>
+		<view class="example-body">
+			<uni-datetime-picker type="datetime" v-model="datetimesingle" start="2021-3-20 12:00:00" end="2021-5-20 20:00:00" />
+		</view>
+		<uni-section :title="'v-model用法：' + single" type="line"></uni-section>
+		<view class="example-body">
+			<uni-datetime-picker v-model="single" />
+		</view>
+		<uni-section :title="'无边框用法：' + single" type="line"></uni-section>
+		<view class="example-body">
+			<uni-datetime-picker v-model="single" :border="false" />
+		</view>
+		<uni-section :title="'disabled用法：' + single" type="line"></uni-section>
+		<view class="example-body">
+			<uni-datetime-picker v-model="single" disabled />
+		</view>
+		<uni-section :title="'日期范围用法：' + '[' + range + ']'" type="line"></uni-section>
+		<view class="example-body">
+			<uni-datetime-picker v-model="range" type="daterange" start="2021-3-20" end="2021-5-20" rangeSeparator="至" />
+		</view>
+		<uni-section :title="'日期时间范围用法：' + '[' + datetimerange + ']' " type="line"></uni-section>
+		<view class="example-body">
+			<uni-datetime-picker v-model="datetimerange" type="datetimerange" start="2021-3-20 12:00:00" end="2021-5-20 20:00:00" rangeSeparator="至" />
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				single: '2021-04-3',
+				'datetimesingle': '',
+				range: ['2021-03-8', '2021-4-20'],
+				datetimerange: ['2021-03-20 20:10:10', '2021-05-10 10:10:10'],
+			}
+		},
+
+		watch: {
+			datetimesingle(newval) {
+				console.log('单选:', this.datetimesingle);
+			},
+			range(newval) {
+				console.log('范围选:', this.range);
+			},
+			datetimerange(newval) {
+				console.log('范围选:', this.datetimerange);
+			}
+		},
+
+		methods: {
+			change(e) {
+				this.single = e
+				console.log('----change事件:', this.single = e);
+			}
+		}
+	}
+</script>
+
+<style>
+	@charset "UTF-8";
+
+	/* 头条小程序组件内不能引入字体 */
+	/* #ifdef MP-TOUTIAO */
+	@font-face {
+		font-family: uniicons;
+		font-weight: normal;
+		font-style: normal;
+		src: url("~@/static/uni.ttf") format("truetype");
+	}
+
+	/* #endif */
+	/* #ifndef APP-NVUE */
+	page {
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		background-color: #efeff4;
+		min-height: 100%;
+		height: auto;
+	}
+
+	view {
+		font-size: 14px;
+		line-height: inherit;
+	}
+
+	.example {
+		padding: 0 15px 15px;
+	}
+
+	.example-info {
+		padding: 15px;
+		color: #3b4144;
+		background: #ffffff;
+	}
+
+	.example-body {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		padding: 0;
+		font-size: 14px;
+		background-color: #ffffff;
+	}
+
+	/* #endif */
+	.example {
+		padding: 0 15px;
+	}
+
+	.example-info {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+		padding: 15px;
+		color: #3b4144;
+		background-color: #ffffff;
+		font-size: 14px;
+		line-height: 20px;
+	}
+
+	.example-info-text {
+		font-size: 14px;
+		line-height: 20px;
+		color: #3b4144;
+	}
+
+	.example-body {
+		flex-direction: column;
+		padding: 15px;
+		background-color: #ffffff;
+	}
+
+	.word-btn-white {
+		font-size: 18px;
+		color: #FFFFFF;
+	}
+
+	.word-btn {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		border-radius: 6px;
+		height: 48px;
+		margin: 15px;
+		background-color: #007AFF;
+	}
+
+	.word-btn--hover {
+		background-color: #4ca2ff;
+	}
+</style>
