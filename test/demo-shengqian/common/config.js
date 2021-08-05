e00b53866bad96d2533e9f92fcdd8bd6b1f2b13a
charles <chao921125@sina.com>
2021-08-06 00:28:56 +0800

    update

--- test/demo-shengqian/common/config.js
+++
@@ -1,9 +0,0 @@
-// const ROOTPATH = "https://tk.gomyorder.cn";
-const ROOTPATH1 = "https://tk.gomyorder.cn/tao";
-const ROOTPATH = "https://tk.gomyorder.cn";
-// const ROOTPATH1 = "http://localhost:8888";
-module.exports = {
-    APIHOST: ROOTPATH,
-    APIHOST1: ROOTPATH1,
-    ROOTPATH: ROOTPATH
-};
--- test/other/src/extend/const-base.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === 'development') {
-  // exports.AXIOS_BASE = 'http://172.19.8.238/api';
-  exports.AXIOS_BASE = 'https://test-trace.advokate-bj.com/api';
-} else {
-  // 正式版
-  exports.AXIOS_BASE = 'https://trace.advokate-bj.com/api';
-}
