54daea7c6931f711e5b5a5872199a1126a6a873c
cc <hcprecog@gmail.com>
2022-08-03 22:34:15 +0800

    update

--- common/config/images.js
+++ common/config/images.js
@@ -1,12 +1,8 @@
-import DemoBg from "@/static/images/demo/demo-bg.jpg";
-import DemoLogo from "@/static/images/demo/demo-logo.jpg";
 import NoData from "@/static/images/toast/img_nodata.png";
 import UserAvatar from "@/static/images/my/user-avatar.png";
 import PaySuccess from "@/static/images/mall/pay/icon_pay_weixin.png";
 
 export default {
-	DemoBg: DemoBg,
-	DemoLogo: DemoLogo,
     NoData: NoData,
     UserAvatar: UserAvatar,
     PaySuccess: PaySuccess,
