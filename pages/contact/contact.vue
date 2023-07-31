4c0e65318094b5e85e80d39e1cab8ed552aba5bb
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-07-31 16:53:30 +0800

    增加获取联系人

--- pages/contact/contact.vue
+++ pages/contact/contact.vue
@@ -1,7 +1,29 @@
 <template>
-	<view>123</view>
+    <view>
+        <button @click="getContact">获取联系人</button>
+    </view>
 </template>
 
-<script></script>
+<script>
+export default {
+    methods: {
+        getContact() {
+            console.log(plus.contacts.ADDRESSBOOK_PHONE);
+            console.log(plus.contacts.ADDRESSBOOK_SIM);
+
+            plus.contacts.getAddressBook(
+                plus.contacts.ADDRESSBOOK_PHONE,
+                function (addressbook) {
+                    // 可通过addressbook进行通讯录操作
+                    alert('Get address book success!', addressbook);
+                },
+                function (e) {
+                    alert('Get address book failed: ' + e.message);
+                }
+            );
+        }
+    }
+};
+</script>
 
 <style lang="scss"></style>
