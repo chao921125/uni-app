ef04f2e15212cb1dc0409ceb681c94f05c1276ec
huangchao <77830871+hc-advokate@users.noreply.github.com>
2023-08-16 17:56:35 +0800

    add

--- common/config/index.js
+++ common/config/index.js
@@ -6,17 +6,17 @@ import color from "./color.js";
 import emoji from "./emoji.js";
 
 export default {
-	color: color,
-	emoji: emoji,
-	baseUrl: request.getBaseUrl(),
-	httpCode: request.httpCode,
-	tokenKey: "user-access-token",
-	userKey: "user-info",
-	historySearchKey: "local-history-search",
-	// 无法动态读取配置，此项作为备选
-	appId: "wxa52bb659b38d1ae2",
-	secret: "8f20daae39de4842271b28c5e8c9a5e9",
-	// 路由去需要添加 / 作为根路径
-	routePath: routerPath,
-	imgPath: imgPath,
+    color: color,
+    emoji: emoji,
+    baseUrl: request.getBaseUrl(),
+    httpCode: request.httpCode,
+    tokenKey: "user-access-token",
+    userKey: "user-info",
+    historySearchKey: "local-history-search",
+    // 无法动态读取配置，此项作为备选
+    appId: "小程序appid",
+    secret: "小程序安全码",
+    // 路由去需要添加 / 作为根路径
+    routePath: routerPath,
+    imgPath: imgPath,
 };
